2e1f660bc10b4fe92fc66e6cb480d9a29f1d317e
Shakthivel Nadar <45127011+Sonic-sakthivel123@users.noreply.github.com>
2019-07-10 20:15:45 +0530

    Update lineage-16.0.sh

--- lineage-16.0.sh
+++ lineage-16.0.sh
@@ -3,6 +3,6 @@
 mkdir -p ~/android/lineage-16.0
 cd ~/android/lineage-16.0
 repo init --depth=1 -u git://github.com/LineageOS/android.git -b lineage-16.0
+git clone git clone git://github.com/team-infusion-developers/android_local_manifest_jfvelte.git -b lineage-16.0
 repo sync -c --force-sync --no-clone-bundle --no-tags
-mkdir -p .repo/local_manifests
-cd
+
